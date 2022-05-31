250fa3ea59aacc89313b6625f256ce3663d380a3
keithchasen <keithchasen89@gmail.com>
2022-05-31 17:27:42 +0300

    sum of items recursively

--- recursion.js
+++ recursion.js
@@ -8,7 +8,7 @@ const factorio = (number) => {
 console.log(factorio(5), 'factorio');
 
 
-// no recursion sum of all elements in a
+// no recursion sum of all elements in array
 
 const sumOfItems = (arr) => {
     let total = 0;
@@ -21,3 +21,19 @@ const sumOfItems = (arr) => {
 const arr = [1, 3, 5, 7, 9];
 
 console.log(sumOfItems(arr), 'sum of items');
+
+
+// Recursion Way to sum all elements in array
+
+const recursionSumOfItems = (arr) => {
+    if(!arr.length)
+        return 0;
+
+    const [currentItem, ...newArr] = arr;
+
+    return currentItem + recursionSumOfItems(newArr);
+}
+
+const arr2 = [2, 4, 6, 8, 10, 12, 14];
+
+console.log(recursionSumOfItems(arr2), 'recursion sum of items');
